bc883eef697bb3bcde8271e8c988176fd2cb0e63
shane <6874@holbertonstudents.com>
2024-01-02 12:17:10 +0100

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from typing import List
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    liste = []
+    for x in range(n):
+        liste.append(await wait_random(max_delay))  # Ajoutez chaque délai à la liste
+    return sorted(liste)
