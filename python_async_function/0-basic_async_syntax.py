9065c546c064df30ac14c779978552fe4be75fae
shane <6874@holbertonstudents.com>
2024-01-03 12:16:08 +0100

    correction pycodestyle

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module shows an asynchronous coroutine wait_random
+"""
+
+
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """takes in an integer argument named wait_random that waits for a random
@@ -8,4 +14,3 @@ async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+"""
+This module shows an fonction
+"""
+
+
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """takes an integer max_delay and returns a asyncio.Task"""
