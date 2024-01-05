9114cf938b062f79197edb26d60c8a320099765c
shane <6874@holbertonstudents.com>
2024-01-05 10:38:01 +0100

    dd

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -16,12 +16,7 @@ async def measure_runtime() -> float:
     measure_runtime should measure the total runtime and return it
     """
     start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(
-        (async_comprehension()),
-        (async_comprehension()),
-        (async_comprehension()),
-        (async_comprehension())
-        )
+    await asyncio.gather(*[async_comprehension() for x in range(4)])
     end_time = asyncio.get_event_loop().time()
     total_time = end_time - start_time
     return total_time
