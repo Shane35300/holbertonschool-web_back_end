92f506d010a4b5aa423e9ba1bd017208907f6235
shane <6874@holbertonstudents.com>
2024-01-22 22:36:33 +0100

    correction of task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	Promise.all([uploadPhoto(), createUser()]).then((results) => {
-		const up_ph = results[0];
-		const cr_us = results[1];
-		const str = up_ph.body + " " + cr_us.firstName + " " + cr_us.lastName;
-		console.log(str);
-	});
+  Promise.all([uploadPhoto(), createUser()]).then((results) => {
+    const upPh = results[0];
+    const crUs = results[1];
+    const str = `${upPh.body} ${crUs.firstName} ${crUs.lastName}`;
+    console.log(str);
+  });
 }
