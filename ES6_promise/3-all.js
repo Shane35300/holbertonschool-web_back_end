b8e12717829dbb3e8f7f62c94fbe9d7e77fd7125
shane <6874@holbertonstudents.com>
2024-01-26 14:48:43 +0100

    cor task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((results) => {
-    const upPh = results[0];
-    const crUs = results[1];
-    const str = `${upPh.body} ${crUs.firstName} ${crUs.lastName}`;
-    console.log(str);
-  })
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const upPh = results[0];
+      const crUs = results[1];
+      const str = `${upPh.body} ${crUs.firstName} ${crUs.lastName}`;
+      console.log(str);
+    })
     .catch(() => {
       console.log('Signup system offline');
     });
