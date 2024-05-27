1072a654802c35dffc73eef7a38d9b45ee8537dc
shane <6874@holbertonstudents.com>
2024-05-27 10:38:07 +0200

    task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" BasiCache """
+""" FIFOCache """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ BasicCache defines a simple caching system that inherits
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a simple caching system that inherits
     from BaseCaching """
 
     def __init__(self):
         """ Initialize """
         super().__init__()
+        self.list = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """ Add an item in the cache (FIFO) """
         if key is None or item is None:
             return
-        else:
+        if key in self.cache_data:
             self.cache_data[key] = item
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                key_to_remove = self.list.pop(0)
+                del self.cache_data[key_to_remove]
+                print(f"DISCARD: {key_to_remove}")
+            self.cache_data[key] = item
+            self.list.append(key)
 
     def get(self, key):
         """ return the value linked to key """
