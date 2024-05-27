8c6376876fea0b06d7ff9f528159f857082b46a3
shane <6874@holbertonstudents.com>
2024-05-27 10:52:09 +0200

    task 2

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" LIFOCache """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache defines a simple caching system that inherits
+    from BaseCaching """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.list = []
+
+    def put(self, key, item):
+        """ Add an item in the cache (LIFO) """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                key_to_remove = self.list.pop(self.MAX_ITEMS - 1)
+                del self.cache_data[key_to_remove]
+                print(f"DISCARD: {key_to_remove}")
+            self.cache_data[key] = item
+            self.list.append(key)
+
+    def get(self, key):
+        """ return the value linked to key """
+        if key is None:
+            return None
+        else:
+            return self.cache_data.get(key)
