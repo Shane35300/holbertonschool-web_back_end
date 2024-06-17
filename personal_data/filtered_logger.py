6f704bef39858b4b5b72f9b1c1f667b9342a6f40
shane <6874@holbertonstudents.com>
2024-06-17 15:58:49 +0200

    task1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,14 +4,32 @@ function called filter_datum that returns the log message obfuscated
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+                    message: str, separator: str) -> str:
     """
     The function should use a regex to replace occurrences of certain field
     values
     """
     pattern = '|'.join(f'{field}=[^ {separator}]+' for field in fields)
     return re.sub(pattern, lambda m: f'{m.group(0).split("=")[0]}={redaction}',
-                  message)
+                message)
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super(RedactingFormatter, self).format(record)
+        obfuscated_message = filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        return obfuscated_message
