a5024ea11924b55711b079caeb410e08a0a69c80
shane <6874@holbertonstudents.com>
2024-06-17 16:11:16 +0200

    task 1 type annot

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -26,7 +26,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """ make a constructor, it updates the class to accept a list of
         strings fields constructor argument"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
