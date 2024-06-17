69b4b8282672b6351829ac0178287c8561b80a27
shane <6874@holbertonstudents.com>
2024-06-17 16:05:10 +0200

    task 1 doc

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -27,10 +27,14 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """ make a constructor, it updates the class to accept a list of
+        strings fields constructor argument"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum. Values
+        for fields in fields should be filtered"""
         original_message = super(RedactingFormatter, self).format(record)
         obfuscated_message = filter_datum(self.fields, self.REDACTION,
                                           original_message, self.SEPARATOR)
