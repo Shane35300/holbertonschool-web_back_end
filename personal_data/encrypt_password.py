8266f2a5fd23c56839d4e6f42257a246cdbd9527
shane <6874@holbertonstudents.com>
2024-06-18 10:44:08 +0200

    task 4

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+function that use the bcrypt package to perform the hashing (with hashpw).
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt."""
+    # Génération d'un sel pour le hachage
+    salt = bcrypt.gensalt()
+    # Hachage du mot de passe avec le sel
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
