b22bae344e8b03b4f83d3169ce95f7dc8e380bf0
shane <6874@holbertonstudents.com>
2024-06-18 12:06:01 +0200

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -14,6 +14,7 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks if a password matches its hashed version."""
     # Vérifie si le mot de passe fourni correspond au mot de passe haché
