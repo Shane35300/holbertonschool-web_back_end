84f2d07295b27e4fc530d52b816c070b87ac31d5
shane <6874@holbertonstudents.com>
2024-06-19 11:14:39 +0200

    task 6

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth for managing API authentication.
-    Currently, this class is empty and serves as a placeholder for future authentication methods.
+    Currently, this class is empty and serves as a placeholder for future
+    authentication methods.
     """
     pass
