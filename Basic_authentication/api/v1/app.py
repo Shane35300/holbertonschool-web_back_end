f7cd2a2d980fd6c732e4538f5c530bb76fa7a86a
shane <6874@holbertonstudents.com>
2024-06-19 10:58:53 +0200

    tassk 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -25,14 +25,19 @@ if auth_type:
 if auth:
     from api.v1.auth.auth import Auth
 
-    @app.before_request
-    def before_request_handler():
-        if not request.path in ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']:
-            if not auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
-                if auth.authorization_header(request) is None:
-                    abort(401)
-                if auth.current_user(request) is None:
-                    abort(403)
+
+@app.before_request
+def before_request_handler():
+    """ This method is called before the request
+    """
+    if auth:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
