b306908d5351817e6125a79c721750e3c7fce45c
shane <6874@holbertonstudents.com>
2024-06-19 12:00:41 +0200

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,9 +7,26 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """
-    BasicAuth class that inherits from Auth for managing API authentication.
-    Currently, this class is empty and serves as a placeholder for future
-    authentication methods.
-    """
-    pass
+    """ BasicAuth class to manage API authentication """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for
+        Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header string.
+
+        Returns:
+            str: The Base64 part of the Authorization header if present and
+            valid, None otherwise.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[len("Basic "):]
