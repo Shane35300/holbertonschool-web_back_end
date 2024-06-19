914568bd3af61d9d386a937fb45e543e6d7abadd
shane <6874@holbertonstudents.com>
2024-06-19 12:44:19 +0200

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ BasicAuth class to manage API authentication.
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -30,3 +31,18 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 part of the Authorization header.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
