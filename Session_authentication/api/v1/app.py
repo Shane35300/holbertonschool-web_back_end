305e74c3fe7d5467c0180adfdd4b729a8472b32a
shane <6874@holbertonstudents.com>
2024-06-21 10:45:49 +0200

    task 0

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -36,8 +36,10 @@ def before_request_handler():
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
+            current_user = auth.current_user(request)
             if auth.current_user(request) is None:
                 abort(403)
+            request.current_user = current_user
 
 
 @app.errorhandler(404)
