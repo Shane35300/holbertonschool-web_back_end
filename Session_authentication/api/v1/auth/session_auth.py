c42e68c325830a298d1bdeb77c7aeb04fc50ebcc
shane <6874@holbertonstudents.com>
2024-06-21 11:09:41 +0200

    task 1 pycode

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,5 +5,11 @@ SessionAuth class to make new authentication mechanism.
 
 from api.v1.auth.auth import Auth
 
+
 class SessionAuth(Auth):
+    """
+    new authentication mechanism:
+    validate if everything inherits correctly without any overloading
+    validate the “switch” by using environment variables
+    """
     pass
