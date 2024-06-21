b81fb300597adedf4cb712ff2bb1c2e744edc911
shane <6874@holbertonstudents.com>
2024-06-21 17:59:20 +0200

    task 10 corrected by antoine

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,29 +65,19 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
 
-    def user_object_from_credentials(self,
-                                     user_email: str,
-                                     user_pwd: str) -> TypeVar('User'):
-        """
-        Returns the User instance based on email and password.
-        """
-        if user_email is None or not isinstance(user_email, str):
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ define user object from credentials """
+        if type(user_email) != str:
             return None
-        if user_pwd is None or not isinstance(user_pwd, str):
+        if type(user_pwd) != str:
             return None
-
-        # Search for the user by email
-        users = User.search({'email': user_email})
-        if not users or len(users) == 0:
-            return None
-
-        user = users[0]
-
-        # Check if the password is valid
-        if not user.is_valid_password(user_pwd):
-            return None
-
-        return user
+        if user_email and user_pwd:
+            users = User.search({"email": user_email})
+            for user in users:
+                if user and user.is_valid_password(user_pwd):
+                    return user
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
