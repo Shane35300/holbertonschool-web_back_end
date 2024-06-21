0e4cb8c208d2a1ee334861ab6d66764655dd6068
shane <6874@holbertonstudents.com>
2024-06-21 12:36:53 +0200

    task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -35,12 +35,13 @@ def before_request_handler():
     """
     if auth:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']
+                          '/api/v1/forbidden/', '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
                 abort(401)
             current_user = auth.current_user(request)
-            if auth.current_user(request) is None:
+            if current_user is None:
                 abort(403)
             request.current_user = current_user
 
