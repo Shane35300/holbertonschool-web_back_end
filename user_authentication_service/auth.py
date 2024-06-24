1d7d109b05e800ab03687fc32a4625d7fe1e4acd
shane <6874@holbertonstudents.com>
2024-06-24 16:43:03 +0200

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,6 +5,31 @@ arguments and returns bytes
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        This method takes mandatory email and password string arguments and
+        return a User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User " + email + " already exists")
+        except NoResultFound:
+            pass
+        user = self._db.add_user(email, _hash_password(password))
+        return user
 
 
 def _hash_password(password: str) -> bytes:
