04a3e154ecf9dbef3c8795098c5080c04ce2c60b
shane <6874@holbertonstudents.com>
2024-06-25 10:10:32 +0200

    task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -17,6 +17,22 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Try locating the user by email. If it exists, check the password with
+        bcrypt.checkpw. If it matches return True. In any other case,
+        return False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                    return True
+                return False
+        except NoResultFound:
+            return False
+
+
     def register_user(self, email: str, password: str) -> User:
         """
         This method takes mandatory email and password string arguments and
