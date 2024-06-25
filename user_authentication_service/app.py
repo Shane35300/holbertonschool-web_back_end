c2c93e46ff416ea70e293fc5e52f2dd21957fea7
shane <6874@holbertonstudents.com>
2024-06-25 09:52:10 +0200

    task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,8 +2,11 @@
 """
 This module create a simple Flask App
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +16,23 @@ def hello_world():
     Simple method for testing
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def user():
+    """
+    Method end-point should expect two form data fields:
+    "email" and "password". If the user does not exist, the end-point should
+    register it and respond with the following JSON payload
+    """
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+    if email and pwd:
+        try:
+            AUTH.register_user(email, pwd)
+            return jsonify({"email": email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
