4c09b8aab5573c6958e8917ade80b7488a6167ee
shane <6874@holbertonstudents.com>
2024-06-25 16:39:47 +0200

    task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -109,5 +109,22 @@ def get_reset_password_token() -> Response:
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> Response:
+    """
+    Update the password. If the token is invalid, catch the exception and
+    respond with a 403 HTTP code. If the token is valid,
+    respond with a 200 HTTP code
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
