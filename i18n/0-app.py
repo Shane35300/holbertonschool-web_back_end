926216407e8a6a7b38f26296dc3838528aff3b15
shane <6874@holbertonstudents.com>
2024-07-02 12:08:29 +0200

    rrrr

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -3,13 +3,18 @@
 Basic Flask app
 """
 from flask import Flask, render_template
+from typing import Any
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello_world() -> Any:
     """
     Home page  with "Hello world" as header
     """
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
