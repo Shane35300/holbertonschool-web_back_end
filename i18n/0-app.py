5c0ad8fb95c77e1c5c48ea91fb0e0049dc84727a
shane <6874@holbertonstudents.com>
2024-07-04 15:37:39 +0200

    ffff

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -2,15 +2,19 @@
 """
 Basic Flask app, All your functions and coroutines must be type-annotated
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from typing import Any
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world() -> Any:
+def hello_world() -> Response:
     """
     Home page  with "Hello world" as header
     """
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
