313ae48deb7e091e40d0c26f01933b444ede2c72
shane <6874@holbertonstudents.com>
2024-07-04 15:18:58 +0200

    task 0

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Basic Flask app
+Basic Flask app, All your functions and coroutines must be type-annotated
 """
 from flask import Flask, render_template
 from typing import Any
@@ -14,7 +14,3 @@ def hello_world() -> Any:
     Home page  with "Hello world" as header
     """
     return render_template('0-index.html')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
