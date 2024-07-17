2eb0dbd4291ac3383ef7c3fef1a6edfb53b33eb2
shane <6874@holbertonstudents.com>
2024-07-17 10:43:27 +0200

    task 8

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,42 +1,21 @@
 const request = require('request');
 const { expect } = require('chai');
-const { exec } = require('child_process');
-const killPort = require('kill-port');
+const app = require('./api');
 
-describe('Index page', function() {
-  let server;
+const BASE_URL = 'http://localhost:7865';
 
-  before(function(done) {
-    killPort(7865)
-      .then(() => {
-        server = exec('node api.js');
-        server.stdout.on('data', (data) => {
-          if (data.includes('API available on localhost port 7865')) {
+describe('Index page', () => {
+    it('should return status code 200', (done) => {
+        request.get(BASE_URL, (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
             done();
-          }
         });
-      })
-      .catch((err) => done(err));
-  });
-
-  after(function(done) {
-    server.kill();
-    done();
-  });
-
-  const url = 'http://localhost:7865/';
-
-  it('should return status 200', function(done) {
-    request(url, function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
     });
-  });
 
-  it('should return the correct message', function(done) {
-    request(url, function(error, response, body) {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+    it('should return the correct message', (done) => {
+        request.get(BASE_URL, (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
     });
-  });
 });
