42a938cbaefaae64c17e1bbaa67f2e43ade01f43
shane <6874@holbertonstudents.com>
2024-07-20 12:48:05 +0200

    task 5

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+
+
+const subscriber = redis.createClient();
+const channelName = 'holberton school channel';
+
+// Événement déclenché lorsque le client se connecte avec succès au serveur Redis
+subscriber.on('connect', () => {
+	console.log('Redis client connected to the server');
+	subscriber.subscribe(channelName);
+});
+
+// Événement déclenché lorsque le client ne parvient pas à se connecter au serveur Redis
+subscriber.on('error', (err) => {
+	console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+subscriber.on('message', (channel, msg) => {
+	if (channel === channelName) {
+		console.log(msg);
+	}
+})
